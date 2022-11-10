0ea5ca28a96d5b30c3e4133b5b602c73d23056ea
Dmitriy Telniy <dmitriy.telniy@ideus.biz>
2022-11-10 17:41:36 +0200

    TS_2022: init

---
+++ src/app.js
@@ -0,0 +1,6 @@
+var a = 10;
+var b = 3;
+function sum(a, b) {
+    console.log(a + b);
+}
+sum(a, b);
---
+++ src/app.ts
@@ -0,0 +1,8 @@
+const a: number = 10;
+const b: number = 3;
+
+function sum(a: number, b: number): void {
+  console.log(a + b);
+}
+
+sum(a, b);
