bb3883d2e005027f5f764ea8f804bd8ef1ff4951
Dmitriy Telniy <dmitriy.telniy@ideus.biz>
2022-12-08 18:11:12 +0200

    TS_2022: Implements (42 lesson)

--- src/app-3(classes).ts
+++ src/app-3(classes).ts
@@ -156,3 +156,40 @@ const user41 = new User41();
 
 user41.login = 'myLogin';
 const t = user41.login;
+
+/**
+ * Implements (42 lesson)
+ */
+
+interface ILogger {
+  log(...args: any): void;
+  error(...args: any): void;
+}
+
+class Logger implements ILogger {
+  log(...args: any[]): void {
+    console.log(...args);
+  }
+  async error(...args: any[]): Promise<void> {
+    console.log(...args);
+  }
+}
+
+interface IPayable {
+  pay(paymentId: number): void;
+  prise?: number;
+}
+
+interface IDeletable {
+  delete(): void;
+}
+
+class User42 implements IPayable, IDeletable {
+  delete(): void {
+    throw new Error('Method not implemented.');
+  }
+  //тип имплементации всегда должен быть таким же или шире (юнион) но не заменять на другой
+  pay(paymentId: number): void {
+    ///
+  }
+}
