0d01e8432bedde24d80a045c2de0b5bbeb112ea8
Dmitriy Telniy <dmitriy.telniy@ideus.biz>
2022-12-19 19:08:05 +0200

    TS_2022: function generic 62 lesson

--- src/app-4(Generics).ts
+++ src/app-4(Generics).ts
@@ -13,3 +13,20 @@ const check61: Record<string, boolean> = {
   kpp: false,
   dasdaasd: true,
 };
+/**
+ * function generic (62 lesson)
+ */
+function logMiddleware<T>(data: T): T {
+  console.log(data);
+  return data;
+}
+
+const res62 = logMiddleware<number>(10);
+
+function getSplitedHalf62<T>(data: T[]): T[] {
+  const l = data.length / 2;
+
+  return data.splice(0, l);
+}
+
+const halflength62 = getSplitedHalf62<number>([4, 5, 6, 7]);
