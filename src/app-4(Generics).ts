ee448ce606f388b392325f3203e027788e5845d5
Dmitriy Telniy <dmitriy.telniy@ideus.biz>
2022-12-20 18:43:13 +0200

    TS_2022:  Generic class (67 lesson)

--- src/app-4(Generics).ts
+++ src/app-4(Generics).ts
@@ -81,3 +81,33 @@ function logId65<T extends string | number, Y>(id: T, additionalData: Y): { id: 
   console.log(additionalData);
   return { id, data: additionalData };
 }
+
+/**
+ * Generic class (67 lesson)
+ */
+//Можно юзать когда нам нужно описать апишку и мы не знаем когда нам придет дата
+class Resp67<D, E> {
+  data?: D;
+  error?: E;
+
+  constructor(data?: D, error?: E) {
+    if (data) {
+      this.data = data;
+    }
+
+    if (error) {
+      this.error = error;
+    }
+  }
+}
+const resp67 = new Resp67<string, number>('data');
+
+class HTTPResp67<F> extends Resp67<string, number> {
+  code: F;
+
+  setCode(code: F) {
+    this.code = code;
+  }
+}
+
+const resp267 = new HTTPResp67();
