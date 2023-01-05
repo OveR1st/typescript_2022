1f2cc6e90dabe37bf3c098fdf1c0a90d64c093d2
Dmitriy Telniy <dmitriy.telniy@ideus.biz>
2023-01-05 21:00:09 +0200

    TS_2022:  Typeof + keyof  (72 lesson)

--- src/app-6(manipulation_types).ts
+++ src/app-6(manipulation_types).ts
@@ -20,3 +20,36 @@ const user70: IUser70 = {
 };
 
 const userName70 = getValue70(user70, 'age');
+
+/**
+ * Typeof (72 lesson)
+ */
+
+let strOrNum72: string | number = 5;
+
+if (Math.random() > 0.5) {
+  strOrNum72 = 5;
+} else {
+  strOrNum72 = '5';
+}
+
+if (typeof strOrNum72 === 'string') {
+  strOrNum72;
+} else {
+  strOrNum72;
+}
+
+let str2orNumber72: typeof strOrNum72; //возьми тип который был у этой переменной
+
+const user72 = {
+  name: 'vas9',
+};
+
+type keyOfUser72 = keyof typeof user72;
+
+enum Direction72 {
+  Up,
+  Down,
+}
+
+type d = keyof typeof Direction72;
