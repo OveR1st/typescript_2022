79044f3d2efc630f089dad4d8bc7f2667e116582
Dmitriy Telniy <dmitriy.telniy@ideus.biz>
2023-01-26 20:26:01 +0200

    TS_2022: Pick, Omit, Extract, Exclude (82 lesson)

--- src/app-7(utility_types).ts
+++ src/app-7(utility_types).ts
@@ -15,3 +15,22 @@ const p81: partial81 = {};
 //Required - обязательно
 type required81 = Required<User81>;
 type required812 = Required<Readonly<User81>>;
+
+/**
+ * Pick, Omit, Extract, Exclude (82 lesson)
+ * под капотом это mapped типы
+ */
+
+interface PaymentPersistent82 {
+  id: number;
+  sum: number;
+  from: string;
+  to: string;
+}
+
+type Payment82 = Omit<PaymentPersistent82, 'id'>;
+
+type PaymentRequisits82 = Pick<PaymentPersistent82, 'from' | 'sum'>;
+
+type ExtractEx82 = Extract<'from' | 'to' | PaymentPersistent82, string>;
+type ExcludeEx82 = Exclude<'from' | 'to' | PaymentPersistent82, string>;
