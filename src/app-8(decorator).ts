e791d593d5a4b3fe78bb0ee65c72108badef7a54
Dmitriy Telniy <dmitriy.telniy@ideus.biz>
2023-02-09 19:25:49 +0200

    TS_2022: Class Decorator (87 lesson)

--- src/app-8(decorator).ts
+++ src/app-8(decorator).ts
@@ -6,7 +6,8 @@ interface IUserService86 {
   users: number;
   getUsersInDatabase(): number;
 }
-
+@threeUserAdvanced87 // вызов второй   (new UserServie86().getUsersInDatabase() - выведет 3)
+@nullUser86 // вызов первый
 class UserServie86 implements IUserService86 {
   users: number = 1000;
 
@@ -15,10 +16,15 @@ class UserServie86 implements IUserService86 {
   }
 }
 
-function nullUser86(obj: UserServie86) {
+function nullUser86(target: Function) {
   //... декоратор это функция которая что то переопределяет у инстанса
-  obj.users = 0;
-  return obj;
+  target.prototype.users = 0;
+}
+
+function threeUserAdvanced87<T extends { new (...args: any): {} }>(constructor: T) {
+  return class extends constructor {
+    users = 3;
+  };
 }
 
 function logUser86(obj: UserServie86) {
@@ -34,4 +40,8 @@ console.log(nullUser86(logUser86(new UserServie86())).getUsersInDatabase()); // 
 
 /**
  * Последовательность применения так же влияет на результат
+ */
+
+/**
+ * Class Decorator (87 lesson)
  */
